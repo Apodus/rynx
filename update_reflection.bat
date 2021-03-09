ff0e35b7f34da8a3866b966ceeb83adaa36a0854
Mika Urtela <mika.urtela@gmail.com>
2021-03-10 00:33:19 +0200

    Add error message on missing libclang.dll

--- update_reflection.bat
+++ update_reflection.bat
@@ -1,7 +1,16 @@
 @echo off
+if not exist ./tools/rynx-codegen/libclang.dll (
+	echo libclang.dll not found.
+	echo download llvm 11.0.0 prebuilt binaries and
+	echo place libclang.dll to ./tools/rynx-codegen/
+	pause
+	exit 1
+)
+
 if exist ./tools/rynx-codegen/rynx-codegen.exe (
 	"./tools/rynx-codegen/rynx-codegen.exe" -I ./src/ --std=c++20 -filename-ends-with components.hpp -gen-for-namespace ::rynx::components -filename-contains components.hpp
 ) else (
 	echo rynx-codegen.exe not found. compile the codegen project first.
+	pause
 	exit 1
 )
