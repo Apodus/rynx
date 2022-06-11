a562198c9ae1a4549f1e0997446189aa38608350
Mika Urtela <mika.urtela@gmail.com>
2022-06-11 19:15:35 +0300

    rewrote type index implementation

--- src/rynx/tech/type_index.cpp
+++ src/rynx/tech/type_index.cpp
@@ -1,5 +1,29 @@
 
 #include <rynx/tech/type_index.hpp>
+#include <algorithm>
+#include <vector>
 
-alignas(std::hardware_destructive_interference_size) std::atomic<uint64_t> rynx::type_index::runningTypeIndex_global = 0;
+uint64_t rynx::type_index::runningTypeIndex_global = 0;
+rynx::type_index::used_type* rynx::type_index::used_type_list = nullptr;
 
+rynx::type_index::type_index() {
+	used_type* current = used_type_list;
+	if (current == nullptr)
+		return;
+
+	std::vector<used_type> types;
+	while (current) { types.emplace_back(*current); current = current->next; }
+	std::sort(types.begin(), types.end(), [](const used_type& a, const used_type& b) { return std::strcmp(a.name, b.name) > 0; });
+	char const* current_name = types.front().name;
+	uint64_t currentId = runningTypeIndex_global;
+	for (auto&& type : types) {
+		if (strcmp(current_name, type.name) != 0) {
+			currentId = ++runningTypeIndex_global;
+			current_name = type.name;
+		}
+		*type.id = currentId;
+	}
+
+	// prevent reinitialization of processed values
+	used_type_list = nullptr;
+}
--- src/rynx/tech/type_index.hpp
+++ src/rynx/tech/type_index.hpp
@@ -1,52 +1,55 @@
 
 #pragma once
 
-#include <rynx/tech/unordered_map.hpp>
-
-#ifndef _WIN32
-namespace std {
-	constexpr size_t hardware_destructive_interference_size = 64;
-}
-#endif
+#include <cstdint>
+#include <type_traits>
 
 namespace rynx {
 	class type_index {
 		
-		alignas(std::hardware_destructive_interference_size) static std::atomic<uint64_t> runningTypeIndex_global;
-		static constexpr uint64_t no_type = ~uint64_t(0);
-
-		// NOTE: As long as we are not using DLLs, this should return the same char* value.
-		//       This means we can use the pointer value instead of the actual string representation for map key.
-#ifdef _WIN32
-		template<typename T> static constexpr char const*const unique_str_for_type() { return __FUNCDNAME__; }
+		struct used_type {
+			used_type* next = nullptr;
+			char const* name = nullptr;
+			uint64_t* id = nullptr;
+		};
+		
+		static used_type* used_type_list;
+		static uint64_t runningTypeIndex_global;
+		
+#ifndef _WIN64
+		template<typename T> static char const* const unique_str_for_t() { return __PRETTY_FUNCTION__; }
 #else
-		template<typename T> static constexpr char const*const unique_str_for_type() { return __PRETTY_FUNCTION__; }
+		template<typename T> static char const* const unique_str_for_t() { return __FUNCSIG__; }
 #endif
-		// all type indices will share values of types. requires static linkage of all use sites.
-		template<typename T> uint64_t id_private__global_ids() const {
-			static uint64_t type_id_value = runningTypeIndex_global++;
-			return type_id_value;
-		}
-
-		template<typename T> uint64_t id_private() const {
-			return id_private__global_ids<T>();
-		}
 
 	public:
+		template<typename T> struct id_for_type {
+			static uint64_t id;
+		};
+
+		template<typename T> static uint64_t mark_type_is_used(id_for_type<T> t) {
+			static used_type used_type_v { used_type_list, unique_str_for_t<T>(), &t.id };
+			used_type_list = &used_type_v;
+			return ~uint64_t(0);
+		}
+
 		struct virtual_type {
 			static constexpr uint64_t invalid = ~uint64_t(0);
 			uint64_t type_value = invalid;
 			bool operator == (uint64_t other) const noexcept { return other == type_value; }
 		};
 
-		type_index() = default;
+		type_index();
 
-		virtual_type create_virtual_type() {
+		virtual_type static create_virtual_type() {
 			return virtual_type{ runningTypeIndex_global++ };
 		}
 
-		template<typename T> uint64_t id() const {
-			return id_private<std::remove_cv_t<std::remove_reference_t<T>>>();
+		template<typename T> static uint64_t id() {
+			using naked_t = std::remove_cv_t<std::remove_reference_t<T>>;
+			return id_for_type<naked_t>::id;
 		}
 	};
 }
+
+template<typename T> uint64_t rynx::type_index::id_for_type<T>::id = rynx::type_index::mark_type_is_used<T>({});
