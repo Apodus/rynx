53c273ec78ee8d8d2088d6a5ada063f4ac6da7c6
Mika Urtela <mika.urtela@gmail.com>
2024-01-23 17:55:15 +0200

    Update to latest sharpmake.

--- generate_codegen_sln.bat
+++ generate_codegen_sln.bat
@@ -1,6 +1,16 @@
 @echo off
-call "./tools/sharpmake/bootstrap.bat" "../../generate/codegen.sharpmake.cs"
-@if %errorlevel% neq 0 goto :error
+pushd %CD%
+
+cd ./tools/Sharpmake/
+dotnet build Sharpmake.sln -c Release
+set SHARPMAKE_EXECUTABLE=%CD%/Sharpmake.Application\bin\Release\net6.0\Sharpmake.Application.exe
+if not exist %SHARPMAKE_EXECUTABLE% echo Cannot find sharpmake executable in %SHARPMAKE_EXECUTABLE% & pause & goto error
+echo Using executable %SHARPMAKE_EXECUTABLE%
+
+popd
+
+echo Updating codegen solution file
+call %SHARPMAKE_EXECUTABLE% /sources(@'./generate/codegen.sharpmake.cs') /outputdir(@'./') /remaproot(@'./') /verbose
 
 @goto :exit
 :error
--- generate_rynx_sln.bat
+++ generate_rynx_sln.bat
@@ -2,7 +2,20 @@
 call ./update_reflection.bat
 echo yup yup
 @if %errorlevel% neq 0 goto :error
-call "./tools/sharpmake/bootstrap.bat" "../../generate/main.sharpmake.cs"
+
+pushd %CD%
+
+cd ./tools/Sharpmake/
+dotnet build Sharpmake.sln -c Release
+set SHARPMAKE_EXECUTABLE=%CD%/Sharpmake.Application\bin\Release\net6.0\Sharpmake.Application.exe
+if not exist %SHARPMAKE_EXECUTABLE% echo Cannot find sharpmake executable in %SHARPMAKE_EXECUTABLE% & pause & goto error
+echo Using executable %SHARPMAKE_EXECUTABLE%
+
+popd
+
+echo Updating game_one solution file
+call %SHARPMAKE_EXECUTABLE% /sources(@'./generate/main.sharpmake.cs') /outputdir(@'./') /remaproot(@'./') /verbose
+
 @if %errorlevel% neq 0 goto :error
 
 @goto :exit
